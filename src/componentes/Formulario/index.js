0eddf9cccb005ce18f0c50729f69620994d63004
GeissonChagas <geisson.excel@gmail.com>
2022-12-31 17:47:26 -0300

    commi

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,8 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from 'react'
 
 
 const Formulario = () => {
@@ -12,14 +14,55 @@ const Formulario = () => {
         'Guias'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Formulário foi submetido! => ', nome, cargo, imagem, time)
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card</h2>
-                <CampoTexto label="Nome:" placeholder="Digite o nome" />
-                <CampoTexto label="Cargo:" placeholder="Digite o cargo" />
-                <CampoTexto label="Imagem:" placeholder="Carregue a imagem" />
-                <ListaSuspensa label="Time:" itens={times} />
+
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome:"
+                    placeholder="Digite o nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo:"
+                    placeholder="Digite o cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+
+                <CampoTexto
+                    label="Imagem:"
+                    placeholder="Carregue a imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time:"
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)} 
+                    />
+
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
 
