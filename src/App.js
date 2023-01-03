9ee5cbb1fca258be7dc7d4bf6c55dde3a63dab79
GeissonChagas <geisson.excel@gmail.com>
2023-01-02 23:26:54 -0300

    criação dos times

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Diretoria',
+      corPrimaria: '#57c278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Financeiro',
+      corPrimaria: '#82cfffa',
+      corSecundaria: 'e8f8ff'
+    },
+    {
+      nome: 'Atendimento',
+      corPrimaria: '#e06869',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'Guias',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff509'
+    }
+  ]
+
   const [membros, setMembros] = useState([])
 
-  const aoNovoMembroAdiciconado = (membro) =>{
+  const aoNovoMembroAdiciconado = (membro) => {
     console.log(membro)
     setMembros([...membros, membro])
   }
@@ -15,7 +39,9 @@ function App() {
     <div className="App">
 
       <Banner />
-      <Formulario aoMembroCadastrado ={membro => aoNovoMembroAdiciconado(membro)} />
+      <Formulario aoMembroCadastrado={membro => aoNovoMembroAdiciconado(membro)} />
+      {times.map(time => <Time nome={time.nome} />)}
+      
 
     </div>
   );
